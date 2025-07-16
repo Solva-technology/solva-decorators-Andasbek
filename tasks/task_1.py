8a6ce714829fe42ce808c38806f752fb67a6fe26
kazybekandas <andasbek.kazybek@gmail.com>
2025-07-16 19:47:01 +0500

    поменял str на repr, добавил обработку на случай если аргументов не передан

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -4,9 +4,13 @@ from functools import wraps
 def log(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        args_str = [str(i) for i in args]
-        kwargs_str = [f"{k}={v}" for k, v in kwargs.items()]
-        all_args = ", ".join(args_str + kwargs_str)
+        if not args and not kwargs:
+            all_args = "без аргументов"
+        else:
+            args_str = [repr(i) for i in args]
+            kwargs_str = sorted([f"{k}={v!r}" for k, v in kwargs.items()])
+            all_args = ", ".join(args_str + kwargs_str)
+
         print(f"Вызов: {func.__name__}({all_args})")
         res = func(*args, **kwargs)
         print(f"Результат: {res}")
