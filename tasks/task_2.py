91281c997b0a72027c8941e69238749a6520cb11
kazybekandas <andasbek.kazybek@gmail.com>
2025-07-16 19:50:34 +0500

    добавил проверку на хешируемость

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,16 +1,34 @@
 from functools import wraps
 
 
+def is_hashable(obj):
+    return (
+        hasattr(obj, "__hash__")
+        and callable(getattr(obj, "__hash__"))
+        and obj.__hash__ is not None
+    )
+
+
 def simple_cache(func):
     cache = {}
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if args in cache:
+        try:
+            key = (args, tuple(sorted(kwargs.items())))
+            if not is_hashable(key):
+                print("Пропуск кэширования: аргументы не хэшируемы")
+                return func(*args, **kwargs)
+        except Exception as e:
+            print(f"Ошибка при создании ключа: {e}")
+            return func(*args, **kwargs)
+
+        if key in cache:
             print("Из кэша")
-            return cache[args]
+            return cache[key]
+
         result = func(*args, **kwargs)
-        cache[args] = result
+        cache[key] = result
         return result
 
     return wrapper
